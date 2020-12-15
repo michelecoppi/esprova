4b95dce0b9ff959fa05c2b463e285de905eae481
Michele <coppi.michele@itismeucci.com>
2020-12-14 23:52:27 -1000

    ciao

---
+++ Daunoadieci.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daunoadieci;
+
+/**
+ *
+ * @author michele2306
+ */
+public class Daunoadieci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=0;
+      for(int i=0; i<10; i++){
+          
+          a++;
+          System.out.println("la variabile è "+a);
+      }
+    }
+    
+}
